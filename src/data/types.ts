0fac647adc94e8f9e5fd0fc8f27679112940b43b
launchsites <jamie@swanweb.co.uk>
2026-03-31 11:47:47 +0100

    better dev setup

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,11 +1,11 @@
 export interface review {
-    id: number;
-    author: string;
-    text: string;
-    rating: number;
-    pfp?: string;
+  id: number;
+  author: string;
+  text: string;
+  rating: number;
+  pfp?: string;
 }
 
-// export interface reviewList {
-//     reviews: review[];
-// }
+export interface reviewList {
+  reviews: review[];
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+export { default as ReviewCard } from "./components/review-card";
+export { default as ReviewGrid } from "./components/review-grid";
+export { default as ReviewCarousel } from "./components/review-carousel";
+
+export type { review, reviewList } from "./data/types";
